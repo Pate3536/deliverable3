f48929523119200471f41a099405ffcc7a94ac60
vinit <vinitpatel2803@gmail.com>
2022-04-23 22:52:23 -0400

    password validator is added

---
+++ src/deliverable3/AIPlayer.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package deliverable3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jeel Patel
+ */
+class AIPlayer extends Player
+
+{
+
+    public ArrayList<Card> queries = new ArrayList<>();
+
+    private int age = 0;
+
+    @Override
+    public void haveTurn()
+
+    {
+
+        boolean playing;
+
+        do{
+
+            Card book = checkForBooks();
+
+            if(book != null)
+
+                System.out.println("Your opponent got a book of " + book + "s...");
+
+            if (hand.isEmpty())
+
+            {
+
+                System.out.print("Your opponent's hand is empty.");
+
+                break;
+
+            }
+
+            Card req = aiMagic();
+
+            System.out.println("Your opponent asks for cards by the name of " + req);
+
+            playing = askFor(req);
+
+            age++;
+
+        } while(playing);
+
+        System.out.println("Your opponent goes fishing.");
+
+        fish();
+
+    }
+
+                //The AI's strategy is to first ask for things you asked for, since you have those things.
+
+                //Failing that, it chooses at random.
+
+    private Card aiMagic()
+
+    {
+
+        if (age>2)
+
+        {
+
+            queries.remove(queries.size()-1);
+
+            age=0;                          
+
+        }
+
+        for(int i=queries.size()-1; i>-1; i--)
+
+            if (hand.contains(queries.get(i)))
+
+            {
+
+                return queries.remove(i);
+
+            }                           
+
+        return hand.get(GoFish.rng.nextInt(hand.size()));
+
+    }
+
+}
---
+++ src/deliverable3/Player.java
@@ -0,0 +1,160 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package deliverable3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jeel Patel
+ */
+abstract class Player
+
+{
+
+    protected ArrayList<Card> hand = new ArrayList<>();
+
+    private int numBooks;
+
+    public Player()
+
+    {
+
+        for(int i=0;i<8;i++)
+
+            fish();
+
+    }
+
+    public boolean hasGiven(Card cType)
+
+    {
+
+        return hand.contains(cType);
+
+    }
+
+    public ArrayList<Card> giveAll(Card cType)
+
+    {
+
+        ArrayList<Card> x = new ArrayList<>();
+
+       for(int i=0;i<hand.size();i++)           
+
+            if (hand.get(i) == cType)
+
+              x.add(hand.get(i));
+
+        for (Card x1 : x) {
+            hand.remove(cType);
+        }
+
+        return x;
+
+  }
+
+    protected boolean askFor(Card cType)
+
+    {
+
+        int tmp = 0;
+
+        if (this instanceof HumanPlayer)
+
+            tmp = 1;
+
+        Player other = GoFish.Players[tmp];
+
+                if (tmp==1)
+
+            ((AIPlayer) other).queries.add(cType);
+
+        //                               //
+
+        if (other.hasGiven(cType))
+
+        {
+
+            for(Card c: other.giveAll(cType))
+
+                hand.add(c);
+
+            return true;
+
+        }
+
+        else
+
+        {
+
+            return false;
+
+        }
+
+    }
+
+    protected final void fish()
+
+                    {
+
+                        if (GoFish.deckSize() > 0)
+
+                                hand.add(GoFish.draw());
+
+                        else
+
+                                System.out.println("But that's impossible since the deck is empty.");
+
+    }
+
+    public int getNumBooks()
+
+    {
+
+        return numBooks;
+
+    }
+
+    protected Card checkForBooks()
+
+    {
+
+        for(Card c: hand)
+
+        {
+
+            int num = 0;
+
+            for(Card d: hand)
+
+              if (c == d)
+
+                  num++;
+
+            if (num == 4)
+
+            {
+
+                for(int i=0;i<4;i++)
+
+                    hand.remove(c);
+
+                numBooks++;
+
+                return c;
+
+            }
+
+        }
+
+        return null;
+
+    }
+
+    public abstract void haveTurn();
+
+}
